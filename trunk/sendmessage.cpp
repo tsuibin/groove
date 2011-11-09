c07d7fa401cff454837e2947cc024b797af7c1e3
topsvr@gmail.com <topsvr@gmail.com@9f519842-37ec-f3b1-1fbc-63351a398776>
2011-11-09 09:04:20 +0000

    窗口居右上

--- trunk/sendmessage.cpp
+++ trunk/sendmessage.cpp
@@ -2,13 +2,21 @@
 #include "message.h"
 #include "workspace.h"
 #include "ui_sendmessage.h"
+#include <QDesktopWidget>
 
 SendMessage::SendMessage(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SendMessage)
 {
     ui->setupUi(this);
+
+    QDesktopWidget* desktop = QApplication::desktop();
+    this->move((desktop->width() - this->width()),0);
+
     this->setWindowTitle("Groove");
+
+    this->setUserInfo();
+
     qDebug() << "con";
 
 }
@@ -41,4 +49,12 @@ void SendMessage::on_pushButton_5_clicked()
      Workspace * workspace = new Workspace();
      workspace->show();
      m_workspaceList << workspace;
+}
+
+void SendMessage::setUserInfo()
+{
+    GSession session;
+
+   // QString uname = b.uname;
+    //ui->label_uinfo->setText("下午好！" + uname);
 }
